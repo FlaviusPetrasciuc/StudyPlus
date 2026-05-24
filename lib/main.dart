9271a18dd482a47e8a9a5cd158635408d6567691
Airaa Srinivasan <airaa.srinivasan@student.nhlstenden.com>
2026-05-24 22:58:00 +0200

    changed from "Myapp" to MainApp - error with test

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:study_plus/pages/createProjectPage/create_project_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: CreateProjectPage() //create project page opens up
-        );
+        debugShowCheckedModeBanner: false,
+        home: CreateProjectPage() //create project page opens up
+    );
   }
 }
